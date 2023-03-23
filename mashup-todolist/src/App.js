bd694cba7f4d0abafe88b89d261e67fb72666511
DESKTOP-SKJSDPL\cha <carisblack4@gmail.com>
2023-03-23 17:01:37 +0900

    23.03.23 todolist 연습

---
+++ mashup-todolist/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import { createGlobalStyle } from 'styled-components';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+      
+    </>
+  );
+}
+
+export default App;
