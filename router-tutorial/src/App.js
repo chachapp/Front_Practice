9dba4afbf69aeb93b2e9e76a9bc4dda4a6977ccd
DESKTOP-SKJSDPL\cha <carisblack4@gmail.com>
2023-03-27 14:55:23 +0900

    23.03.27 라우팅, 서브라우팅

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Routes>
-        <Route path='/' Component={Home}></Route>
-        <Route path='/about' Component={About}></Route>
+       <Route path="/" exact={true} element={<Home/>} />
+       <Route path="/about" element={<About/>} />
+       <Route path="/profiles/*" element={<Profiles/>} />
+
 
       </Routes>
-      
+
     </div>
   );
 }
