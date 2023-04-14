45f2c1e22919a97160c754a1f5ad2a4c5ee49712
DESKTOP-SKJSDPL\cha <carisblack4@gmail.com>
2023-04-14 14:36:37 +0900

    23.04.14 react

---
+++ board_practice/src/App.tsx
@@ -0,0 +1,7 @@
+import Hello from "./Hello";
+
+function App() {
+  return <Hello name="react"/>
+}
+
+export default App;
---
+++ board_practice/src/Hello.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Hello(props : string) {
+    return <div>안녕하세요 {props}</div>
+}
+
+export default Hello;
