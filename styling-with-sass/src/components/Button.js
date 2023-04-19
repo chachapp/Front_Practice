a280ffd366f9f72ba980785c995560c769d92aa2
DESKTOP-SKJSDPL\cha <carisblack4@gmail.com>
2023-04-19 15:43:43 +0900

    230419. sass 연습

--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 import "./Button.scss";
 
-function Button({ children }) {
+function Button({ children, size }) {
     return (
-        <button className='Button'>{children}</button>
+        <button className={classNames('Button', size)}>{children}</button> // className에 동적으로 클래스이름 넣기
     )
 }
+
+Button.defaultProps = {
+    size: 'medium'
+}
+
 
 export default Button;
